8e2fb200aae39e66fd33134971dd459c848fb091
jaeson-dev <jaeson.dev@gmail.com>
2022-03-07 20:01:44 +0900

    Initial Commit

---
+++ menu_tutorial/menu_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_tutorialApp.swift
+//  menu_tutorial
+//
+//  Created by Jaeson.dev on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct menu_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
